19da20a713f3e9e3c65064eded5cab67ffdd5b08
codervijay143 <vk9677022@gmail.com>
2026-03-29 08:18:17 +0530

    backend initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/message", (req, res) => {
+  res.json({
+    message: "Backend connected successfully 🚀",
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Vijay" },
+    { id: 2, name: "React" },
+    { id: 3, name: "Node" },
+  ]);
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
